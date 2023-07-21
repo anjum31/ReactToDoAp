94a3b8f4d55c3f5866c279aae1b5377edbb30216
anjum31 <sadiaanjum31028@gmail.com>
2023-07-21 19:05:06 +0600

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const TaskInput = ({handleAdd}) => {
+  const [list,setList] = useState("");
+
+  const handleInputChange = (e) => {
+    setList(e.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter new task"
+        value={list}
+        onChange={handleInputChange}
+      />
+      <button onClick={() => {
+        if (list) {
+          handleAdd(list);
+          setList("");
+        }
+      }}>Add Here the Task List</button>
+    </div>
+  );
+};
+
+const TaskList = ({lists, handleDeleteTask }) => {
+  return (
+    <ul>
+      {lists.map((list, index) => (
+        <li key={index}>
+          {list}
+          <button onClick={() => handleDeleteTask(index)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const App = () => {
+  const [lists, setLists] = useState([]);
+
+  const handleAddTask = (list) => {
+    setLists([...lists, list]);
+  };
+
+  const handleDeleteTask = (index) => {
+    const newTasks = lists.filter((x, i) => i !== index);
+    setLists(newTasks);
+  };
+
+  return (
+    <div>
+      <TaskInput handleAdd={handleAdd} />
+      <TaskList tasks={tasks} handleDeleteTask={handleDeleteTask} />
+    </div>
+  );
+};
+
+export default App;
